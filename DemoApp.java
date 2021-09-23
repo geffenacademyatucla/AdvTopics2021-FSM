23a4792a21a63c0b6427c16f4e26e9751cea4337
Geffen Academy Computer Science <rspell@geffenacademy.ucla.edu>
2021-09-23 09:58:31 -0700

    added comments

--- DemoApp.java
+++ DemoApp.java
@@ -2,6 +2,13 @@ import processing.core.PApplet;
 import processing.core.PVector;
 import java.util.HashMap;
 
+/**
+ * Application to demonstrate controlling an application with a StateMachine
+ * Notice that the application has no logic to control the visuals, just setup procedures.
+ * All of the logic resides in the States
+ * @author rspell
+ *
+ */
 public class DemoApp extends PApplet {
 	
 	// Static Attributes
@@ -10,11 +17,18 @@ public class DemoApp extends PApplet {
 	public static final Integer LOWERLEFT = 2;
 	public static final Integer LOWERRIGHT = 3;
 	
+	// This is a place holder reference so that classes that need to access
+	// processing methods can do so
 	public static PApplet ctx;
 	
 	// Attributes
+	// An Array of MouseAreas that cover the canvas of the application
 	MouseArea[] mArea;
+	
+	// Object to manage the state of the application
 	RegionStateMachine sm;
+	
+	// The current MouseArea where the mouse is located
 	MouseArea hoverMouseArea;
 	
 	public static void main(String[] args) {
@@ -33,33 +47,33 @@ public class DemoApp extends PApplet {
 		mArea[LOWERLEFT] = new MouseArea(height*0.5,height,0,width*0.5,color(200,0,0), color(0,200,0),LOWERLEFT);
 		mArea[LOWERRIGHT] = new MouseArea(height*0.5, height, width*0.5, width, color(0,200,0), color(200,0,0),LOWERRIGHT);
 		
-		//initial mouseArea is the upper left
-		hoverMouseArea = mArea[0];
+		//initial hoverMouseArea is the upper left
+		hoverMouseArea = mArea[UPPERLEFT];
 		
-
 		// create State Machine
 		sm = new RegionStateMachine(this);
+		// set the global state for the state machine
 		sm.setGlobalState(GlobalRegionState.getInstance());
+		// initialize the current state
 		sm.setCurrentState(UpperLeftAnimateState.getInstance());
 
 	}
 	
 	public void draw() {
 		background(255);
-	
+		
+		// everything happens in here...
 		sm.update();
 
 	}
+
 	
-	public void mouseClicked() {
-		PVector f = new PVector(10,3);
-//		vehicle.applyForce(f);
-	}
-	
-	Region currentState() {
-		return null;
-	}
-	
+	/**
+	 * Convenience method to save typing when calling System.out.printf
+	 * 
+	 * @param formatString
+	 * @param args
+	 */
 	public static void printf(String formatString, Object ... args ) {
 		System.out.printf(formatString, args);
 	}
